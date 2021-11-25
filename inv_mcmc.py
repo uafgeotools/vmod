6901b4e3e550b32195214e43feccb909a234bf0a
mfangaritav <mf.angarita52@gmail.com>
2021-11-25 03:50:53 +0000

    updated mcmc

--- inv_mcmc.py
+++ inv_mcmc.py
@@ -9,61 +9,76 @@ import pymc
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-def inversion(source,data):
+def inversion(source,data,wts=1.0):
     parnames=source.get_parnames()
+    #print(data)
     def model(data):
         #Distribution for every parameter
         theta=[]
         orders=[]
         for i in range(source.get_num_params()):
             order=int(np.log10(np.max([np.abs(source.low_bounds[i]),np.abs(source.high_bounds[i])])))-1
-            low=source.low_bounds[i]/(10**order)
-            high=source.high_bounds[i]/(10**order)
-            ini=source.x0[i]/(10**order)
-            #print(low,high,ini,order)
+            if not parnames[i]=='pressure':
+                low=source.low_bounds[i]/(10**order)
+                high=source.high_bounds[i]/(10**order)
+                ini=source.x0[i]/(10**order)
+            else:
+                low=np.log10(source.low_bounds[i])
+                high=np.log10(source.high_bounds[i])
+                ini=np.log10(source.x0[i])
             thetat=pymc.Uniform(parnames[i],low,high,value=ini)
             theta.append(thetat)
         model_pars=theta
         sigma=pymc.Uniform('sigma',0,1,value=0.5)
-        #sigma=100
+        
         #Deformation is the deterministic variable
         @pymc.deterministic(plot=False)
         def defo(model_pars=model_pars):
-            return source.get_model_reduced(model_pars)
+            return source.get_model(model_pars)
 
         #Probability distribution
-        z = pymc.Normal('z', mu=defo, tau=1.0/sigma, value=data, observed=True)
+        z = pymc.Normal('z', mu=defo, tau=1.0*wts/sigma, value=data, observed=True)
         return locals()
 
     #Making MCMC model
     MDL = pymc.MCMC(model(data))
 
-    #Choosing Metropolis-Hastings for step method
-    #MDL.use_step_method(pymc.AdaptiveMetropolis,MDL.model_pars,scales={MDL.model_pars[0]:1000,MDL.model_pars[1]:1000})
+    #Choosing Adaptive Metropolis-Hastings for step method
     MDL.use_step_method(pymc.AdaptiveMetropolis,MDL.model_pars)
-    #MDL.use_step_method(pymc.Metropolis,MDL.model_pars)
+    
+    #Steps, burnin and thining for each model
     if source.get_source_id()=='Mogi':
-        steps=110000
+        steps=1100000
         burnin=10000
-        thin=100
+        thin=1000
+    elif source.get_source_id()=='Mctigue':
+        steps=1100000
+        burnin=10000
+        thin=1000
+    elif source.get_source_id()=='Nishimura':
+        steps=11000
+        burnin=1000
+        thin=10
     elif source.get_source_id()=='Okada':
         steps=1100000
         burnin=10000
         thin=1000
     elif source.get_source_id()=='Penny':
-        steps=41000
-        burnin=1000
-        thin=100
-    elif source.get_source_id()=='Yang':
         steps=110000
         burnin=10000
+        thin=100
+    elif source.get_source_id()=='Yang':
+        steps=1100000
+        burnin=100000
         thin=1000
+        
     #Number of runs
     MDL.sample(steps,burnin,thin) 
+    
+    #Getting the traces
     traces=[]
     for i in range(source.get_num_params()):
         traces.append(MDL.trace(parnames[i]))
-    #traces.append(MDL.trace('sigma'))
     traces=np.array(traces)
     return traces,MDL
 
