d24046d53eac12e8f36707a68410da650912b2fa
mfangaritav <mf.angarita52@gmail.com>
2023-04-23 23:14:44 +0000

    Changes in edm class

--- vmod/data/edm.py
+++ vmod/data/edm.py
@@ -23,84 +23,75 @@ class Edm(Data):
         self.zends=None
         super().__init__()
         
-    def add_deltas(self,delta):
-        self.add_comp(delta,'delta')
-        self.delta=delta
+    def add_ts(self,ts):
+        self.assert_size(np.concatenate((ts,ts)),'ts')
+        self.ts=ts
         
-    def add_coords(self,coords,obj,origin=True):
-        #The assert checks that coords has the same length as obj
-        self.assert_origins_ends(coords)
-        if obj is None:
-            obj=coords
-        elif origin:
-            if len(obj)==len(coords):
-                obj=coords+obj
-            else:
-                obj[0:len(coords)]=coords
-        else:
-            if len(obj)==len(coords):
-                obj=obj+coords
-            else:
-                obj[len(coords):2*len(coords)]=coords
-        return obj
+    def add_deltas(self,delta):
+        self.assert_origins_ends(delta)
+        self.data=delta
+        self.err=delta*0+1
+        self.comps=[delta]
+        self.delta=delta
     
     def add_xorigins(self,origins):
         self.assert_origins_ends(origins)
         self.xorigins=origins
-        newcoords=self.add_coords(self.xorigins,self.xs,origin=True)
-        self.add_xs(newcoords)
+        if not self.xends is None:
+            self.add_xs(np.concatenate((self.xorigins,self.xends)))
         
     def add_xends(self,ends):
         self.assert_origins_ends(ends)
         self.xends=ends
-        newcoords=self.add_coords(self.xends,self.xs,origin=False)
-        self.add_xs(newcoords)
+        if not self.xorigins is None:
+            self.add_xs(np.concatenate((self.xorigins,self.xends)))
         
     def add_yorigins(self,origins):
         self.assert_origins_ends(origins)
         self.yorigins=origins
-        newcoords=self.add_coords(self.yorigins,self.ys,origin=True)
-        self.add_ys(newcoords)
+        if not self.yends is None:
+            self.add_ys(np.concatenate((self.yorigins,self.yends)))
         
     def add_yends(self,ends):
         self.assert_origins_ends(ends)
         self.yends=ends
-        newcoords=self.add_coords(self.yends,self.ys,origin=False)
-        self.add_ys(newcoords)
+        if not self.yorigins is None:
+            self.add_ys(np.concatenate((self.yorigins,self.yends)))
         
     def add_lls_lines(self,lons,lats,origin=True,ori=None):
         if origin==True:
             self.lonsori=lons
             self.latsori=lats
-            if not self.lonsend is None:
-                lons=np.concatenate((self.lonsori,self.lonsend))
-                lats=np.concatenate((self.latsori,self.latsend))
-                self.add_lls(lons,lats,ori=ori)
         else:
             self.lonsend=lons
             self.latsend=lats
-            if not self.lonsori is None:
-                lons=np.concatenate((self.lonsori,self.lonsend))
-                lats=np.concatenate((self.latsori,self.latsend))
-                self.add_lls(lons,lats,ori=ori)
+            
+        if (not self.lonsori is None) and (not self.lonsend is None):
+            lons=np.concatenate((self.lonsori,self.lonsend))
+            lats=np.concatenate((self.latsori,self.latsend))
+            self.add_lls(lons,lats,ori=ori)
+            self.xorigins=self.xs[0:int(len(lons)/2)]
+            self.xends=self.xs[int(len(lons)/2):len(lons)]
+            self.yorigins=self.ys[0:int(len(lons)/2)]
+            self.yends=self.ys[int(len(lons)/2):len(lons)]
     
     def add_zorigins(self,origins):
         self.assert_origins_ends(origins)
         self.zorigins=origins
-        newcoords=self.add_coords(self.zorigins,self.zs,origin=True)
-        self.add_zs(newcoords)
+        if not self.zends is None:
+            self.add_zs(np.concatenate((self.zorigins,self.zends)))
         
     def add_zends(self,ends):
         self.assert_origins_ends(ends)
         self.zends=ends
-        newcoords=self.add_coords(self.zends,self.zs,origin=False)
-        self.add_zs(newcoords)
+        if not self.zorigins is None:
+            self.add_zs(np.concatenate((self.zorigins,self.zends)))
         
-    def get_num_points(self):
+    def get_num_lines(self):
         params=[self.xorigins,self.yorigins,self.zorigins,self.xends,self.yends,self.zends,self.ts,self.data]
         for i,l in enumerate(params):
             if isinstance(l,(list,np.ndarray)):
-                if i==len(params)-1:
+                if i in [len(params)-2,len(params)-1]:
                     return len(l)/self.get_size_per_point()
                 else:
                     return len(l)
@@ -108,7 +99,7 @@ class Edm(Data):
         
     def assert_origins_ends(self,arr):
         assert isinstance(arr,(list,np.ndarray)),'It should be an array'
-        assert len(arr)==self.get_num_points() or self.get_num_points()==None,'The size does not correspond to the postions size'
+        assert len(arr)==self.get_num_lines() or self.get_num_lines()==None,'The size does not correspond to the postions size'
         
     def from_model3d(self,func,unravel=True):
         ux1,uy1,uz1=func(self.xorigins,self.yorigins)
